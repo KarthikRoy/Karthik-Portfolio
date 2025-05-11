b55aa0bd1302348c3e810e2912fe541d971ee7c6
Tharun Tej <125950708+tharuncruz7@users.noreply.github.com>
2025-05-11 12:52:36 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+  strings:["Structural Design", "Construction Management", "Geotechnical Engineering", "Surveying & Planning", "Sustainable Infrastructure"],
+  typeSpeed : 10,
+  backSpeed : 100,
+  backDelay : 1000,
+  loop : true
+});
